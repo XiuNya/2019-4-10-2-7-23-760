1c75cb005d7cfb5c9c82d7577b892eb51469abc9
lihan <lihan@cug.edu.cn>
2019-05-26 21:41:53 +0800

    the Taximeter

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var flagDownFare=6;
+    var tripFare=0.8;
+    var flagFallDistance=2;
+    var remoteDistance=8;
+    var priceMakup=0.5;
+    var waitingFare=0.25;
+    function basePrice(distance) {
+      if(distance<=flagFallDistance)
+        return flagDownFare;
+      if(distance>remoteDistance)
+        return flagDownFare+(remoteDistance-flagFallDistance)*tripFare+(distance-remoteDistance)*(1+priceMakup)*tripFare;
+      return flagDownFare+(distance-flagFallDistance)*tripFare;
+    };
+    function waitingPrice(parkTime) {
+      return waitingFare*parkTime;
+    };
+    function totalPrice(basePrice,waitingPrice) {
+      return Math.round(basePrice+waitingPrice);
+    };
+
+    var basePrice=basePrice(inputs.distance);
+    var waitingPrice=waitingPrice(inputs.parkTime);
+    var price=totalPrice(basePrice,waitingPrice);
+    //return "price";
+    return price;
 };
